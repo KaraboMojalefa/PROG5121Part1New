a48060cc1a7c67b0b2d2556a6e9b4185a1df22a2
Student <Student@LabVM2049939.0c04slomk3uehisk0khwhxw52d.jnbx.internal.cloudapp.net>
2026-09-07 19:17:48 +0200

    Added Login class

---
+++ src/main/java/com/mycompany/prog5121part1new/Login.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prog5121part1new;
+
+/**
+ *
+ * @author Student
+ */
+public class Login {
+    
+    //stores the user's details needed for the login and registration parts
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;//it will contain the + international code
+}
