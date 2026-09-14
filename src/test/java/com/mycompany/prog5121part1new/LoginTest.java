cd8231d35d668f2795d7599a3bbe9a197ae95ea8
Student <Student@LabVM2049939.0c04slomk3uehisk0khwhxw52d.jnbx.internal.cloudapp.net>
2026-09-14 10:52:59 +0200

    Added login test class

---
+++ src/test/java/com/mycompany/prog5121part1new/LoginTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+package com.mycompany.prog5121part1new;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+    
+}
