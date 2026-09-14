98defb836094b8ac3ccbc1aa89bbde6b9daca98f
Student <Student@LabVM2049939.0c04slomk3uehisk0khwhxw52d.jnbx.internal.cloudapp.net>
2026-09-14 20:23:28 +0200

    Added Scanner to main

---
+++ src/main/java/com/mycompany/prog5121part1new/PROG5121Part1New.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121part1new;
+
+/*
+allows us to use Scanner to get information typed by the user
+*/
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class PROG5121Part1New {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+    }
+}
